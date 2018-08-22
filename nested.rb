7679553754c86b460a842bb8d1d296522ab5899d
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-22 17:51:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -61,11 +61,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-  programmer_hash[:yukihiro_matsumoto] =
-    {
-     :known_for => "Ruby",
-     :languages => ["LISP", "C"]
-    }
+  
 
 	programmer_hash = 
  		{
@@ -83,7 +79,11 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] =
+    {
+     :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+    }
 end
 
 def changing_alan
